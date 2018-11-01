2c38b0a2075ff40f81630a7a08dab454852a37da
Robin Eklund <robineklund6@gmail.com>
2018-11-01 22:15:07 +0200

    First commit

---
+++ src/Config/config.js
@@ -0,0 +1,8 @@
+export const DB_CONFIG = {
+  apiKey: 'AIzaSyDK3F7vBSQoTTubfDhdbJxM1JWdGnYPEQY',
+  authDomain: 'react-notes-87d6f.firebaseapp.com',
+  databaseURL: 'https://react-notes-87d6f.firebaseio.com',
+  projectId: 'react-notes-87d6f',
+  storageBucket: 'react-notes-87d6f.appspot.com',
+  messagingSenderId: '37858892040'
+};
