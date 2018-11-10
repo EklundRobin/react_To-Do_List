e07fba41f8ba7e7737983042037d8146fd261ba6
Robin Eklund <robineklund6@gmail.com>
2018-11-10 15:02:34 +0200

    Removed my own apikey from file

--- src/Config/config.js
+++ src/Config/config.js
@@ -1,8 +1,3 @@
 export const DB_CONFIG = {
-  apiKey: 'AIzaSyDK3F7vBSQoTTubfDhdbJxM1JWdGnYPEQY',
-  authDomain: 'react-notes-87d6f.firebaseapp.com',
-  databaseURL: 'https://react-notes-87d6f.firebaseio.com',
-  projectId: 'react-notes-87d6f',
-  storageBucket: 'react-notes-87d6f.appspot.com',
-  messagingSenderId: '37858892040'
+  // Key from your database
 };
